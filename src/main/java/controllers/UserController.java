e11843bdbe4bd5460ff647d31fc7fb5f522e2b21
Nuno Soares <nunosoaresdev@gmail.com>
2020-02-01 12:49:11 +0000

    basic upload image

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -5,11 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import repositories.UserRepository;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Optional;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 public class UserController {
@@ -17,10 +22,26 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    private static String UPLOAD_FOLDER = "src/main/resources/img/";
+
     @PostMapping (value = "/register", produces = "application/json")
     public ResponseEntity<User> register(@RequestBody User user){
         User newUser = userRepository.save(user);
         return new ResponseEntity("User register sucessfully!", HttpStatus.OK);
+    }
+
+    @PostMapping (value = "/users/upload")
+    public ResponseEntity uploadImage(@RequestParam("file")MultipartFile file){
+     //   String filename = file.getOriginalFilename();
+        byte[] filename = new byte[0];
+        try {
+            Path path = Paths.get(UPLOAD_FOLDER + file.getOriginalFilename());
+            Files.write(path, filename);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity(filename, HttpStatus.OK);
     }
 
     @GetMapping(value = "/users", produces = "application/json")
